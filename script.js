0765a99aded827c28be1b5a0e75d7b5f7a5ffcfa
MKcode-45 <kalotamishael@gmail.com>
2024-11-19 14:36:24 +0200

    Added a hover effect to the grid
    Using the mouseover event listener, I added a hover
    effect to the grid that changes the background color of
    the individual grid squares to blue when hovered on.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let number = 16;
 for(let i = 0; i < number*number; i++){
     const div = document.createElement("div");
     container.appendChild(div);
-    div.style.cssText = "width: 55px; height: 52px; border: 1px solid red;"
+    div.style.cssText = "width: 55px; height: 52px; border: 1px solid red;";
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "blue";
+    } )
 }
 
