20eb5b278f1fceffb6436fe48f2dd189222e95e3
MKcode-45 <kalotamishael@gmail.com>
2024-11-20 09:57:56 +0200

    Added the ability for users to set the grid size.
    Created a function that uses the appropriate DOM
    methods, to remove all the grid squares
    once the reset button is clicked, clicking the
    reset button also allows users to set their own
    desired grid size within the specified range.

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 const container = document.querySelector(".container");
+
 let number = 16;
 
-for(let i = 0; i < number*number; i++){
-    const div = document.createElement("div");
-    container.appendChild(div);
-    div.style.cssText = "width: 55px; height: 52px; border: 1px solid red;";
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "blue";
-    } )
+function makeGrid (){
+    for(let i = 0; i < number*number; i++){
+        const div = document.createElement("div");
+        container.appendChild(div);
+        div.style.cssText = "width: 55px; height: 52px; border: 1px solid red;";
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "blue";
+        } )
+    }
 }
+
+makeGrid ();
+
+function deleteChild() {
+    let child = container.lastElementChild;
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+}
+
+
+const button = document.querySelector("#reset");
+button.addEventListener("click", () => {
+    number = prompt("Enter a number between 1 - 100.");
+    number = Number(number);
+    deleteChild();
+    makeGrid ();
+});
+
+
+
 
